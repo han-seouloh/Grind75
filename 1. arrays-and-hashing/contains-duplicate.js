147b0b1ce6de9d00a55246e6e3c607ab75b2d29b
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-03-27 23:55:27 -0600

    Solution to contains duplicate

---
+++ 1. arrays-and-hashing/contains-duplicate.js
@@ -0,0 +1,18 @@
+duplicate = [1,2,3,4,1];
+noDuplicate = [1,2,3,4,5];
+
+const containsDuplicate = (nums) => {
+  const set = new Set(); // A set stores unique values. (A value cannot be repeated in a set)
+
+  for (const num of nums) { // This loops over every number in the array.
+    if (set.has(num)) return true; // If the number is already in the set, a duplicate was found.
+
+    set.add(num); // If not, then add the number to the set.
+  }
+
+  return false // If we manage to get out of the loop, then no duplicates were found.
+}
+
+
+console.log('There\'s a duplicate: ' + containsDuplicate(duplicate));
+console.log('There\'s a duplicate: ' + containsDuplicate(noDuplicate));
