fb48805ed2061d5427ede3508892bb1e4614304e
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-04-04 14:29:53 -0600

    Refactor code for easier comprehension and add comments

--- 3.stack/evaluate-reverse-polish-notation.js
+++ 3.stack/evaluate-reverse-polish-notation.js
@@ -3,43 +3,44 @@ const test = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"];
 
 var evalRPN = function(tokens) {
   const numStack = []; // Initialize stack to store numbers.
-  let result, // Variable to store result
-      top; //
+  let result, // Variable to store result.
+      top,  // Variable to store top value of stack.
+      next; // Variable to store next value after top value in stack.
 
-  if(tokens.length === 1) {
-      return Number(tokens[0])
+  if(tokens.length === 1) { // If the token is only one number.
+      return Number(tokens[0]) // Return said number.
   }
 
   
-  for (let i = 0; i < tokens.length; i++) {
-      if (Number(tokens[i]) || Number(tokens[i]) === 0) {
-          numStack.push(Number(tokens[i]));
-      } else {
-          top = numStack.pop();
-          result = numStack.pop();
+  for (let i = 0; i < tokens.length; i++) { // Loop through tokens array
+      if (Number(tokens[i]) || Number(tokens[i]) === 0) { // If the token is a number,
+          numStack.push(Number(tokens[i])); // push said number to the stack.
+      } else { // If it is a token,
+          top = numStack.pop(); // top = first value popped from the stack.
+          next = numStack.pop(); // next = second value popped from the stack.
 
-          switch (tokens[i]) {
-              case '+':
-                  result = result + top;
+          switch (tokens[i]) { // Switch case for the detected token.
+              case '+': // In case it is a sum,
+                  result = next + top; // Add both numbers.
                   break;
-              case '-':
-                  result = result - top;
+              case '-': // In case it is a subtraction,
+                  result = next - top; // Subtract both numbers.
                   break;
-              case '*':
-                  result = result * top;
+              case '*': // In case it is a multiplication,
+                  result = next * top; // Multiply both numbers.
                   break;
-              default:
-                  if (top/result > 0) {
-                      result = Math.floor(result / top);
+              default: // In case it is a division,
+                  if ( next/top > 0) { // This is to round the numbers towards zero.
+                      result = Math.floor(next / top); // Divide both numbers.
                   } else {
-                      result = Math.ceil(result / top);
+                      result = Math.ceil(next / top);
                   }
                   break;
           }
-          numStack.push(result);         
+          numStack.push(result); // Push the result back to the stack. 
       }
   }
-  return result;
+  return result; // Return result.
 }
 
 console.log(evalRPN(test));
