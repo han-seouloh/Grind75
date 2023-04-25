885f49bf18458fe9c70e130de6ec6bb50db7e111
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-04-25 14:46:12 -0600

    Finish reverse linked list

---
+++ 6.linked-lists/reverse-linked-list.js
@@ -0,0 +1,15 @@
+// Leetcode Problem #206 Reverse Linked List
+
+var reverseList = function (head) {
+  let [ prev, curr, next ] = [ null, head, null ];
+
+  while (curr) {/* Time O(N) */
+      next = curr.next;
+      curr.next = prev;
+
+      prev = curr;
+      curr = next;
+  }
+
+  return prev;
+};
